363ffeeabbd0a7d0dad06d6cec2280952b9aa90e
Geekyume <aminaouini565@gmail.com>
2023-11-27 20:48:50 +0100

    setup invalid url error

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const busboyBodyParser = require('busboy-body-parser')
+const fs = require('fs')
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 4000;
 
 app.use(cors());
-
+app.use(busboyBodyParser())
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +20,14 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res)=>{
+  let {url} = req.body;
+  let isValid = url.includes('http://') || url.includes('https://')
+  if(!isValid) res.json({error: "Invalid URL"})
+  
+  res.redirect("/")
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
