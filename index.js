52056dc27db07eb73e81d7ebf6877b5f35db4aaf
Geekyume <aminaouini565@gmail.com>
2023-11-27 21:24:46 +0100

    if url already in db send short url

--- index.js
+++ index.js
@@ -25,8 +25,17 @@ app.post('/api/shorturl', (req, res)=>{
   let {url} = req.body;
   let isValid = url.includes('http://') || url.includes('https://')
   if(!isValid) res.json({error: "Invalid URL"})
-  
-  res.redirect("/")
+
+  fs.readFile(__dirname + "/db/urls.json", (error, urls) =>{
+    if (error) res.json({error})
+    urls = JSON.parse(urls)
+    
+    if(typeof urls[url] === 'number') res.json({ original_url : url, short_url : urls[url]})
+    var index = Object.keys(urls).length;
+      res.json(index)
+    
+  })
+  //res.redirect("/")
 })
 
 app.listen(port, function() {
